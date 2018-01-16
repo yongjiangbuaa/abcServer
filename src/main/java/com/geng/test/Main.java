5367c8d41cc3e1424c7aec88e20228ed4d44253a
gengyongjiang <yongjiangbuaa@gmail.com>
2018-01-16 11:25:15 +0800

    add slfj

--- src/main/java/com/geng/test/Main.java
+++ src/main/java/com/geng/test/Main.java
@@ -1,8 +1,14 @@
 package com.geng.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Main {
 
 	public static void main(String[] args){
 		System.out.println("hello!");
+		Logger logger = LoggerFactory.getLogger(Main.class);
+		logger.info("this is logged by SLF4J");
 
 	}
 

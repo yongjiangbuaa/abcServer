dfe94face53c3160ad3accbb47fa33d91a414130
gengyongjiang <yongjiangbuaa@gmail.com>
2018-01-16 11:54:48 +0800

    add logback status print

--- src/main/java/com/geng/test/Main.java
+++ src/main/java/com/geng/test/Main.java
@@ -1,5 +1,7 @@
 package com.geng.test;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +11,8 @@ class Main {
 		System.out.println("hello!");
 		Logger logger = LoggerFactory.getLogger(Main.class);
 		logger.info("this is logged by SLF4J");
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+		StatusPrinter.print(lc);
 
 	}
 

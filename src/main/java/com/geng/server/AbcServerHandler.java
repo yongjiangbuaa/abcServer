216925f15fff88dda82915f388279d25eb8af0fb
gengyongjiang <yongjiangbuaa@gmail.com>
2018-01-19 16:45:08 +0800

    http server

--- src/main/java/com/geng/server/AbcServerHandler.java
+++ src/main/java/com/geng/server/AbcServerHandler.java
@@ -2,20 +2,66 @@ package com.geng.server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static io.netty.buffer.Unpooled.copiedBuffer;
+
 public class AbcServerHandler extends ChannelInboundHandlerAdapter {
     private  final Logger logger = LoggerFactory.getLogger(AbcServerHandler.class);
+
+//    @Override
+//    public void channelActive(ChannelHandlerContext ctx){
+//        ByteBuf time = ctx.alloc().buffer(4);
+//        time.writeInt(Math.toIntExact(System.currentTimeMillis()/1000L));
+//        final ChannelFuture f = ctx.writeAndFlush(time);
+//        f.addListener(new ChannelFutureListener() {
+//            @Override
+//            public void operationComplete(ChannelFuture future) throws Exception {
+//                    assert f == future;
+//                    ctx.close();
+//            }
+//        });
+//
+//    }
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)  {
-        ByteBuf in = (ByteBuf)msg;
-        logger.info("recieved data:{}",in.toString(CharsetUtil.UTF_8));
-        ctx.write(in);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //业务逻辑开始
+        if(msg instanceof FullHttpRequest){
+             final FullHttpRequest request = (FullHttpRequest) msg;
+             final String responseMessage = "gengyongjiang has got yout http post!! params is deviceId:";
+//            String content = request.content();
+            FullHttpResponse response = new DefaultFullHttpResponse(
+                    HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK,
+                    copiedBuffer(responseMessage.getBytes()));
+            if (HttpHeaders.isKeepAlive(request))
+            {
+                response.headers().set(
+                        HttpHeaders.Names.CONNECTION,
+                        HttpHeaders.Values.KEEP_ALIVE
+                );
+            }
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE,
+                    "text/plain");
+            response.headers().set(HttpHeaders.Names.CONTENT_LENGTH,
+                    responseMessage.length());
+
+            ctx.writeAndFlush(response);
+
+
+        }else{
+            super.channelRead(ctx,msg);
+        }
+//        ByteBuf in = (ByteBuf)msg;
+//        logger.info("recieved data:{}",in.toString(CharsetUtil.UTF_8));
+//        ctx.write(in);
     }
 
     @Override
@@ -28,6 +74,11 @@ public class AbcServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
+        ctx.writeAndFlush(new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                copiedBuffer(cause.getMessage().getBytes())
+        ));
         cause.printStackTrace();
         logger.error(cause.getMessage());
         ctx.close();
--- src/main/java/com/geng/server/Server.java
+++ src/main/java/com/geng/server/Server.java
@@ -7,11 +7,14 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 //协议处理
 public class Server {
     private int port;
     public Server(int port){
+//        Runtime.getRuntime().addShutdownHook();//JVM关闭时关闭资源？
         this.port = port;
     }
     public void run() throws Exception {
@@ -23,8 +26,10 @@ public class Server {
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new AbcServerHandler());
+                        protected void initChannel(SocketChannel ch)  {
+                            ch.pipeline().addLast("codec",new HttpServerCodec());
+                            ch.pipeline().addLast("aggregator",new HttpObjectAggregator(512*1024));
+                            ch.pipeline().addLast("request",new AbcServerHandler());
                         }
                     }).option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);

de7bf1bc66d0d614c6e5b23e1c4e23f1bb0f1392
gengyongjiang <yongjiangbuaa@gmail.com>
2018-01-25 15:19:58 +0800

    add column level

--- install/march.sql
+++ install/march.sql
@@ -4,6 +4,7 @@ CREATE TABLE `user_profile` (
   `gold` int	DEFAULT '0' COMMENT '金币',
   `star` int	DEFAULT '0' COMMENT '星',
   `heartTime`	bigint	DEFAULT '0' COMMENT '生命恢复倒计时',
+  `level`	int DEFAULT '1' COMMENT '关卡', 
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- install/update.sql
+++ install/update.sql
@@ -1,3 +1,13 @@
+CREATE TABLE `user_profile` (
+  `uid` varchar(40) COLLATE utf8_unicode_ci NOT NULL COMMENT '玩家id',
+  `heart`int	DEFAULT  '0' COMMENT  '命',
+  `gold` int	DEFAULT '0' COMMENT '金币',
+  `star` int	DEFAULT '0' COMMENT '星',
+  `heartTime`	bigint	DEFAULT '0' COMMENT '生命恢复倒计时',
+  `level`	int DEFAULT '1' COMMENT '关卡', 
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE `uid_bind` (
   `uid` varchar(40) COLLATE utf8_unicode_ci NOT NULL COMMENT '玩家id',
   `bindId` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT '绑定类型id',

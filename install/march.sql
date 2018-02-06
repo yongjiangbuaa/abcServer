52190bc7e61d5757f926dac57110c57c457e3076
gengyongjiang <yongjiangbuaa@gmail.com>
2018-02-06 17:37:56 +0800

    add story sql

--- install/march.sql
+++ install/march.sql
@@ -28,3 +28,16 @@ CREATE TABLE `user_item` (
   PRIMARY KEY (`uuid`),
   KEY `uitem_ownerId_index` (`ownerId`,`itemId`,`value`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+//剧情
+CREATE TABLE `user_story` (
+  `uuid` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `ownerId` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `storyId` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `subId` int(11) NOT NULL DEFAULT '0',
+  `type` int(11) DEFAULT NULL,
+  `updateTime` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`uuid`),
+  KEY `uitem_ownerId_index` (`ownerId`,`storyId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+

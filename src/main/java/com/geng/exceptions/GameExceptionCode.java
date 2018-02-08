c16173e41c15594e088416c88640a6491d07b63b
gengyongjiang <yongjiangbuaa@gmail.com>
2018-02-08 11:39:42 +0800

    comment fo error code

--- src/main/java/com/geng/exceptions/GameExceptionCode.java
+++ src/main/java/com/geng/exceptions/GameExceptionCode.java
@@ -1,11 +1,11 @@
 package com.geng.exceptions;
 
 public enum GameExceptionCode {
-    UID_NOT_EXIST("10001"),
-    INVALID_OPT("E00000"),
-    ITEM_NOT_ENOUGH("10003"),
-    LIFE_NOT_ENOUGH("10004"),
-    USERGOLD_IS_NOT_ENOUGH("10005"),
+    UID_NOT_EXIST("10001"),//uid不存在
+    INVALID_OPT("E00000"),//无效操作 通常是参数错误
+    ITEM_NOT_ENOUGH("10003"),//物品不足
+    LIFE_NOT_ENOUGH("10004"),//heart不足
+    USERGOLD_IS_NOT_ENOUGH("10005"),//金币不足
     ;
 
     public String getValue() {

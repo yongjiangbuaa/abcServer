842c2df8984fd8716af71b0910bcbb9695a1a9ce
gengyongjiang <yongjiangbuaa@gmail.com>
2018-02-24 18:50:38 +0800

    testP ok

--- testP.sh
+++ testP.sh
@@ -3,13 +3,10 @@ echo 1=$1
 WWW=123.206.90.153:9933
 LOCAL=10.1.33.220:8080
 IP=$LOCAL
-if [ -n $2 ]
-then
-	IP=$WWW
-fi
-CMD="curl -d '$1' -X POST http://$IP > testPotocalLog"
+#if [ -n $2 ]
+#then
+#	IP=$WWW
+#fi
+CMD="curl -d '$1' -X POST http://$IP "
 echo "CMD=$CMD"
 `$CMD`
-RES=`cat ./testPotocalLog`
-
-echo $RES
